262ee3200261a2e5647d251e929d625bd470ac54
Ashish Kumar <askumaryg@gmail.com>
2021-01-11 22:20:39 +0530

    implement Sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );
